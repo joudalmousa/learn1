0e1f88115ef50803ae9d8f7a1802a4b21f6b6b34
joud <almousajoud@gmail.com>
2025-10-23 14:43:40 +0300

    Initial Commit

---
+++ learn1/learn1App.swift
@@ -0,0 +1,17 @@
+//
+//  learn1App.swift
+//  learn1
+//
+//  Created by joud almousa on 01/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct learn1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
